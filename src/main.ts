9fd8dd56e7e8e497eb083ca0908281a1efa6cc8a
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2023-06-07 14:47:56 -0400

    Add Project Angular 16

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { bootstrapApplication } from '@angular/platform-browser'
+import { AppComponent } from '@app/app.component'
+import { provideRouter, Routes } from '@angular/router'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { httpInterceptor } from '@shared/interceptors/http.interceptor'
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/form',
+        pathMatch: 'full',
+    },
+    {
+        path: 'form',
+        loadComponent: () => import('./app/components/form/form.component').then((m) => m.FormComponent),
+    },
+]
+bootstrapApplication(AppComponent, {
+    providers: [provideHttpClient(withInterceptors([httpInterceptor])), provideRouter(routes)],
+}).catch((err) => console.error(err))
