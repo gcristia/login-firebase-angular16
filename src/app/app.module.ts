e089b7eb2802a42966868c7166568e796fe971e9
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2023-06-07 15:44:17 -0400

    Add Firebase Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app'
+import { environment } from '@environments/environment'
+import { getAuth, provideAuth } from '@angular/fire/auth'
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+    ],
     providers: [provideHttpClient()],
     bootstrap: [AppComponent],
 })
